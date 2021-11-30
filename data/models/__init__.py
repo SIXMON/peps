32b964fdb99009c90fee714e89eeaa4f5f06f459
Célestin <ceudeline@sixmon.net>
2021-11-30 16:58:31 +0100

    feat(data) : Create Productions Model

--- data/models/__init__.py
+++ data/models/__init__.py
@@ -2,4 +2,5 @@ from .farmer import Farmer, FarmImage
 from .experiment import Experiment, ExperimentImage, ExperimentVideo, TAGS
 from .message import Message
 from .cultures import CULTURES
+from .productions import PRODUCTIONS
 from .theme import Theme
